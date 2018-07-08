d79ce2545cdc2a9dfae27a316c199aeb3afc272f
Cloud User <ec2-user@ip-172-31-23-182.us-west-2.compute.internal>
2018-07-08 08:43:39 +0000

    include image

--- app.py
+++ app.py
@@ -362,7 +362,13 @@ def handle_message(event):
         #line_bot_api.reply_message(
         #    event.reply_token,
         #    message)
-        line_bot_api.replySticker(event.reply_token, { packageId: '1', stickerId: '1' })
+        image_message = ImageSendMessage(
+            original_content_url='https://charleswang.us/opensuse-line-bot/taiwan-tech2.jpg',
+            preview_image_url='https://charleswang.us/opensuse-line-bot/taiwan-tech1.png'
+        )
+        line_bot_api.reply_message(
+            event.reply_token, image_message)
+        #line_bot_api.replySticker(event.reply_token, { packageId: '1', stickerId: '1' })
         return 0
     if event.message.text == "YouTube":
         target_url = 'https://www.youtube.com/user/opensusetv/videos'
